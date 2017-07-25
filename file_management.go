093dac008ca16ca72b7b7736ab362532d1fbf3df
Franco Ferra <Franco Ferra>
2017-07-25 00:52:04 -0300

    Added file management functions

--- file_management.go
+++ file_management.go
@@ -1,15 +1,97 @@
 package frango
 
 import (
+	"archive/zip"
+	"io"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func CreateFile(filePath string, content string) {
+func CreateFolder(folderPath string) {
+	os.Mkdir(folderPath, 0777)
+}
+
+func CreateFile(filePath string, content string) error {
 	file, err := os.Create(filePath)
-	PrintErr(err)
+	if err != nil {
+		return err
+	}
+
 	file.WriteString(content)
+
+	return nil
+}
+
+func DeleteFile(filePath string) error {
+	if err := os.Remove(filePath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ZipFile(filePath string, target string) error {
+	zipFile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+
+	archive := zip.NewWriter(zipFile)
+	defer archive.Close()
+
+	info, err := os.Stat(filePath)
+	if err != nil {
+		return nil
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		baseDir = filepath.Base(filePath)
+	}
+
+	filepath.Walk(filePath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		if baseDir != "" {
+			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, filePath))
+		}
+
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		writer, err := archive.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = io.Copy(writer, file)
+		return err
+	})
+
+	return err
 }
 
 func ServeFile(c *gin.Context, filePath string, fileName string) {
