dc9e279cd5ebfd4c0b2b043551059739672bb391
Franco Ferraguti <franco@MacBook-Pro-de-Macbook-2.local>
2017-07-25 12:44:34 -0300

    Added ParseRequestBody

--- common.go
+++ common.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+func ParseRequestBody(c *gin.Context) map[string]interface{} {
+	body, _ := ioutil.ReadAll(c.Request.Body)
+	var data map[string]interface{}
+
+	if err := json.Unmarshal(body, &data); err != nil {
+		return data
+	}
+
+	return data
+}
+
 func PrintErr(err error) {
 	if err != nil {
 		fmt.Println(err.Error())
