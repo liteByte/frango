b1ceeb52085f6d99326c6daf67961425be4bf9ec
Franco Ferra <Franco Ferra>
2017-08-25 01:51:24 -0300

    RemoveLastCharacters & RemoveFirstCharacters functions

--- string_management.go
+++ string_management.go
@@ -13,3 +13,13 @@ func FirstLetterToLower(s string) string {
 	bytes := StringToByteArray(s)
 	return strings.ToLower(string(bytes[0])) + string(bytes[1:len(bytes)])
 }
+
+func RemoveFirstCharacters(s string, n int) string {
+	bytes := StringToByteArray(s)
+	return string(bytes[n:len(bytes)])
+}
+
+func RemoveLastCharacters(s string, n int) string {
+	bytes := StringToByteArray(s)
+	return string(bytes[0 : len(bytes)-n])
+}
