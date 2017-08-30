751ee3dc4c976d28a5c886c3e16a20406570eb08
Franco Ferra <Franco Ferra>
2017-08-29 23:35:32 -0300

    Added PrintSQLQuery function

--- common.go
+++ common.go
@@ -1,10 +1,13 @@
 package frango
 
 import (
+	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -30,4 +33,47 @@ func PrintErr(err error) {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+}
+
+func PrintSQLQuery(query string, args ...interface{}) string {
+	var buffer bytes.Buffer
+	nArgs := len(args)
+	for i, part := range strings.Split(query, "?") {
+		buffer.WriteString(part)
+		if i < nArgs {
+			switch a := args[i].(type) {
+			case int64:
+				buffer.WriteString(fmt.Sprintf("%d", a))
+			case bool:
+				buffer.WriteString(fmt.Sprintf("%t", a))
+			case sql.NullBool:
+				if a.Valid {
+					buffer.WriteString(fmt.Sprintf("%t", a.Bool))
+				} else {
+					buffer.WriteString("NULL")
+				}
+			case sql.NullInt64:
+				if a.Valid {
+					buffer.WriteString(fmt.Sprintf("%d", a.Int64))
+				} else {
+					buffer.WriteString("NULL")
+				}
+			case sql.NullString:
+				if a.Valid {
+					buffer.WriteString(fmt.Sprintf("%q", a.String))
+				} else {
+					buffer.WriteString("NULL")
+				}
+			case sql.NullFloat64:
+				if a.Valid {
+					buffer.WriteString(fmt.Sprintf("%d", a.Float64))
+				} else {
+					buffer.WriteString("NULL")
+				}
+			default:
+				buffer.WriteString(fmt.Sprintf("%q", a))
+			}
+		}
+	}
+	return buffer.String()
 }
