924fc31f4383c45ded877a96452546544dd2ae2e
Franco Ferra <Franco Ferra>
2017-10-30 22:48:17 -0300

    AWS

---
+++ aws_management.go
@@ -0,0 +1,25 @@
+package frango
+
+import (
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func PrintS3Buckets(region string) {
+	svc := s3.New(session.New(&aws.Config{Region: aws.String(region)}))
+
+	result, err := svc.ListBuckets(&s3.ListBucketsInput{})
+	if err != nil {
+		log.Println("Failed to list buckets", err)
+		return
+	}
+
+	log.Println("Buckets:")
+	for _, bucket := range result.Buckets {
+		log.Printf("%s : %s\n", aws.StringValue(bucket.Name), bucket.CreationDate)
+	}
+}
