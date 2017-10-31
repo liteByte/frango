aa81e9fa4737141b31ae061963ab8797bc1ebbfb
Franco Ferra <Franco Ferra>
2017-10-30 23:07:55 -0300

    S3 File Upload

--- aws_management.go
+++ aws_management.go
@@ -1,11 +1,13 @@
 package frango
 
 import (
+	"io"
 	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func PrintS3Buckets(region string) {
@@ -22,4 +24,20 @@ func PrintS3Buckets(region string) {
 	for _, bucket := range result.Buckets {
 		log.Printf("%s : %s\n", aws.StringValue(bucket.Name), bucket.CreationDate)
 	}
+}
+
+func UploadImageToS3(file io.Reader, region, bucketName, fileName string) string {
+	uploader := s3manager.NewUploader(session.New(&aws.Config{Region: aws.String(region)}))
+
+	result, err := uploader.Upload(&s3manager.UploadInput{
+		Body:   file,
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(fileName),
+	})
+
+	if err != nil {
+		log.Fatalln("Failed to upload file to S3: ", err)
+	}
+
+	return result.Location
 }
